2d17cd57679174ba4d6346e7d48005c226d654d1
abdo0433 <abedobedo8@gmail.com>
2026-04-23 14:22:51 +0200

    Update cv.js

--- routes/cv.js
+++ routes/cv.js
@@ -9,7 +9,7 @@ const User = require("../models/User");
 
 const upload = multer({ dest: "uploads/" });
 
-const FASTAPI_URL = "http://ai.messi-messi-qyvdq7-c401a0-130-162-46-17.traefik.me";;
+const PYTHON_URL = process.env.PYTHON_URL;
 
 const authMiddleware = (req, res, next) => {
   const authHeader = req.header("Authorization");
